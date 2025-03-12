232c569bf717c81a86b8a3f5cef5b12ad746a8ff
ZHO-ZHO-ZHO <140084057+ZHO-ZHO-ZHO@users.noreply.github.com>
2025-03-13 00:59:18 +0800

    Update custom_adapter.py

--- toolkit/custom_adapter.py
+++ toolkit/custom_adapter.py
@@ -25,10 +25,10 @@ import random
 sys.path.append(REPOS_ROOT)
 from typing import TYPE_CHECKING, Union, Iterator, Mapping, Any, Tuple, List, Optional, Dict
 from collections import OrderedDict
-from ipadapter.ip_adapter.attention_processor import AttnProcessor, IPAttnProcessor, IPAttnProcessor2_0, \
+from ip_adapter.attention_processor import AttnProcessor, IPAttnProcessor, IPAttnProcessor2_0, \
     AttnProcessor2_0
-from ipadapter.ip_adapter.ip_adapter import ImageProjModel
-from ipadapter.ip_adapter.resampler import Resampler
+from ip_adapter.ip_adapter import ImageProjModel
+from ip_adapter.resampler import Resampler
 from toolkit.config_modules import AdapterConfig, AdapterTypes
 from toolkit.prompt_utils import PromptEmbeds
 import weakref
